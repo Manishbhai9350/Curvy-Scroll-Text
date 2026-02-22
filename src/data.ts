6da78ab6229f2437953e133f52622dcf1a708612
Manishbhai9350 <keshraram294@gmail.com>
2026-02-22 18:18:43 +0530

    first commit

---
+++ src/data.ts
@@ -0,0 +1,44 @@
+const Data = {
+  address: {
+    heading: "Address",
+    content: [
+      "Seventeen",
+      "19-20 Great Sutton Street",
+      "London",
+      "EC1V 0DR",
+      "UK",
+    ],
+  },
+
+  currentTime: {
+    heading: "Current Time",
+    content: ["12:27:25 (GMT)"],
+  },
+
+  businessEmail: {
+    heading: "Business Enquiry Email",
+    content: ["amelia@seventeenagency.com"],
+  },
+
+  generalEmail: {
+    heading: "General Enquiries",
+    content: ["hello@seventeenagency.com"],
+  },
+
+  jobsEmail: {
+    heading: "Job Enquiries",
+    content: ["hiring@seventeenagency.com"],
+  },
+
+  phone: {
+    heading: "Telephone",
+    content: ["+44 (0) 20 7242 5017"],
+  },
+
+  usaRepresentation: {
+    heading: "USA Representation",
+    content: ["jeff@atrbute.com"],
+  },
+};
+
+export default Data;
